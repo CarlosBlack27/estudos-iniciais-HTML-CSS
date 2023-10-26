3f3b36b6d3cdbd1aec966a9185502d11e9da4567
CarlosBlack27 <128640414+CarlosBlack27@users.noreply.github.com>
2023-10-26 13:55:09 -0300

    Add files via upload

---
+++ jogos.py
@@ -0,0 +1,21 @@
+import forca
+import adivinhacao
+
+def escolhe_jogo():
+    print("*********************************")
+    print("escolha o seu jogo!")
+    print("**********************************")
+
+    print("(1) forca, (2) adivinhaçã")
+
+    jogo = int(input("qual jogo"))
+
+    if (jogo == 1):
+        print("jogando forca")
+        forca.jogar()
+    elif (jogo == 2):
+        print("jogando adivinhação")
+        adivinhacao.jogar()
+
+if(__name__=="__main__"):
+    escolhe_jogo()
